b9ee3fac611e47db733ec793cb5d25d0d40b8639
Raakka <jrip03@gmail.com>
2020-09-08 21:09:07 -0400

    more advanced hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    for i in 1..11 {
+        if i == 1 {
+            println!("{} Rust server is better than express", i);
+        } else {
+            println!("{} Rust servers are better than express", i);
+        }
+    }
 }
